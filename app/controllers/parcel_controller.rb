0d78f686f3fa6af72880e4be1f6937bb62380822
Nikita Matsnev <niki-sim@mail.ru>
2021-06-24 21:59:19 +0300

    add price and distance to view

--- app/controllers/parcel_controller.rb
+++ app/controllers/parcel_controller.rb
@@ -1,4 +1,14 @@
 class ParcelController < ApplicationController
   def index
   end
+
+  def new
+    @from_city = City.find_by(name: params[:from_city_name])
+    @to_city = City.find_by(name: params[:to_city_name])
+    @distance = 230 # request here
+    @result = 500 + (@distance * (params[:volume].to_i + params[:weigth].to_i))
+    # logger.info ("log" + @price.to_s)
+    # redirect_to root_path
+    render :index
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root "parcel#index"
+
+  get "/", to:"parcel#index"
+  post "/",  to:"parcel#new"
 end
